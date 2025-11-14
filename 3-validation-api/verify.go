0e14b0d68712c1c84d51a91230c476e0598cd5e7
Daniel Kosovskiy <churkabes420@gmail.com>
2025-11-14 22:10:12 +0300

    1st attempt at random-api lesson

--- 3-validation-api/verify.go
+++ 3-validation-api/verify.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"github.com/jordan-wright/email"
+	"encoding/json"
+	"fmt"
+	"io"
+	rand2 "math/rand/v2"
 	"net/http"
 	"net/smtp"
+	"os"
 )
 
-type Email struct {
-	text string
+type User struct {
+	Email    string `json:"Email"`
+	Password string `json:"Password"`
+	Address  string `json:"Address"`
+	Hash     string `json:"Hash"`
 }
 
 func NewEmailHandler(Mux *http.ServeMux) {
@@ -17,7 +24,31 @@ func NewEmailHandler(Mux *http.ServeMux) {
 
 }
 func (m Email) Send(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
+	fmt.Println("Received user, sending verification request...")
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Error reading request body: ", err)
+		_, err = w.Write([]byte("Error reading request body"))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	data := Email{}
+	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling json: ", err)
+	}
+	if data.Email == "" {
+		fmt.Println("Error, empty email address")
+		_, err = w.Write([]byte("Error, empty email address"))
+		if err != nil {
+			fmt.Println(err)
+		}
+	} else {
+		hash := MakeUser(data)
+		address := fmt.Sprint("http://localhost:8081/Verify/", hash)
+		User{Email: data.Email}.CreateAndSendEmail(address)
+	}
 
 }
 
@@ -25,11 +56,33 @@ func (m Email) Verify(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func CreateAndSendEmail(text string) {
-	e := email.NewEmail()
-	e.From = "Daniel Kosovskiy <broiler747tb@gmail.com>"
-	e.To = []string{"churkabes420@gmail.com"}
-	e.Subject = "Verification Email"
-	e.Text = []byte(text)
-	e.Send("smtp.gmail.com:587", smtp.PlainAuth("Daniel", "broiler747tb@gmail.com", "xxx", "smtp.gmail.com"))
+func (u User) CreateAndSendEmail(text string) {
+	mail := CreateEmailConfig(u.Address)
+	mail.Text = []byte(text)
+	err := mail.Send("smtp.gmail.com:587", smtp.PlainAuth("Daniel", "broiler747tb@gmail.com", "xxx", "smtp.gmail.com"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func GenerateHash() string {
+	var hash string
+	for i := 0; i < 31; i++ {
+		hash = hash + string(rune(rand2.Int32N(25)+97))
+	}
+	return hash
+}
+
+func MakeUser(mail Email) string {
+	hash := GenerateHash()
+	usr := User{Email: mail.Email, Hash: hash}
+	bytes, err := json.Marshal(usr)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = os.WriteFile("user.json", bytes, 0775)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return hash
 }
