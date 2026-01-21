748f3c2fe5d43244a07ad28df4557740ae9b39af
Daniel Kosovskiy <churkabes420@gmail.com>
2026-01-21 14:48:26 +0300

    Save

--- 4-order-api/auth/handler.go
+++ 4-order-api/auth/handler.go
@@ -22,8 +22,8 @@ func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 	handler := AuthHandler{
 		Config: deps.Config,
 	}
-	router.HandleFunc("POST /auth/login", handler.Login)
-	router.HandleFunc("POST /auth/register", handler.Register)
+	router.HandleFunc("POST /auth/login", handler.Login())
+	router.HandleFunc("POST /auth/register", handler.Register())
 }
 
 func (handler *AuthHandler) Login() http.HandlerFunc {
